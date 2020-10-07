0f6582efbe2ea8e6b1a02aef40182114e3f7a171
helping083 <helping083@gmail.com>
2020-10-07 14:17:59 +0300

    checkout-base-markup

--- src/pages/Checkout/Checkout.component.jsx
+++ src/pages/Checkout/Checkout.component.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectCardItems, selectCardTotal} from '../../store/card/card.selectors';
 import './checkout.styles.scss';
 
 class Checkout extends Component {
@@ -10,12 +13,42 @@ class Checkout extends Component {
   }
 
   render() {
+    const {cartItems, total} = this.props;
     return (
-      <div>
-        <p>checkot</p>
+      <div className='checkout-page'>
+        <div className='checkout-header'>
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
+        </div>
+        {
+          cartItems.map(cartItem => 
+            cartItem.name
+          )
+        }
+        <div className='total'>
+          <span>TOTAL: ${total}</span>
+        </div>
       </div>
     )
   }
 }
 
-export default Checkout
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCardItems,
+  total: selectCardTotal
+});
+
+export default connect(mapStateToProps,null)(Checkout);
--- src/store/card/card.selectors.js
+++ src/store/card/card.selectors.js
@@ -16,3 +16,8 @@ export const selectCartItemsCount = createSelector(
   [selectCardItems],
   cardItems => cardItems.reduce((prev, curr) => prev + curr.quantity, 0)
 )
+
+export const selectCardTotal = createSelector(
+  [selectCardItems],
+  cardItems => cardItems.reduce((prev, curr) => prev + curr.quantity * curr.price, 0)
+)
